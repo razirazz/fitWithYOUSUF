67b1586586c1eb216bee10c647a5790def6f8834
razirazz <razeenarazz2001@gmail.com>
2025-08-22 20:31:35 +0530

    services first page done

--- src/components/textempasize.tsx
+++ src/components/textempasize.tsx
@@ -2,8 +2,8 @@
 
 const TextEmphasize = (props) => {
     return (
-        <div className="relative p-px rounded-md">
-            <div className="relative bg-(--background) text-(--highlight-color) p-2 pl-3 pr-3 rounded-md z-2
+        <div className="relative p-px rounded-md w-fit h-fit">
+            <div className="relative bg-(--background) text-(--highlight-color) p-1 md:p-2 pl-2 md:pl-3 pr-2 md:pr-3 rounded-md z-2
                             overflow-hidden">
                 {props.data}
             </div>
@@ -19,7 +19,7 @@ const TextEmphasize = (props) => {
             </div> */}
             </>
             <div className="overflowHider absolute w-full h-full top-0 left-0 overflow-hidden rounded-md">
-                <div className="glaze absolute -bottom-6 -left-10 h-[100px] w-[20vw] rounded-md z-1
+                <div className="glaze absolute -bottom-6 -left-10 h-[100px] w-[50vw] md:w-[20vw] rounded-md z-1
                             bg-linear-[-65deg] from-(--highlight-color) via-(--text-color)
                             from-40% via-50% to-60% to-(--highlight-color) animate-glaze-linear">
                 </div>
--- src/pages/services.tsx
+++ src/pages/services.tsx
@@ -13,28 +13,29 @@ const ServicesPage = () => {
                         for 
                         <span className="text-lg sm:text-2xl"><TextEmphasize data="Every Goal" /></span>
                     </div>
-                    <div className="services-title-content text-center text-sm font-[family-name:--font-body] sm:text-md sm:w-[60vw]
+                    <div className="relative services-title-content text-center text-sm font-[family-name:--font-body] sm:text-md sm:w-[60vw]                                    
                                     tracking-wide sm:tracking-wider">
                         Whether you're looking to 
-                        lose fat
+                        lose fat,
                         ,
-                        gain muscle
-                        ,
-                        recover
                         from an injury, or simply build a
-                        healthier routine
                         , I offer
-                        structured
                         ,
-                        science-backed
                         programs tailored to your individual needs. With over
-                        8 years
                         of experience and a
-                        Level 4
                         certification, I blend proven training methods with
-                        personalized coaching
                         to help you succeed — wherever you are in your journey
                     </div>
+                    {/* 
+                        <div className="text-sm"><TextEmphasize data="lose fat" /></div> 
+                        <div className="text-sm"><TextEmphasize data="gain muscle" /></div>
+                        <div className="text-sm"><TextEmphasize data="recover" /></div>
+                        <span className="text-sm"><TextEmphasize data="healthier routine" /></span>
+                        <span className="text-sm"><TextEmphasize data="structured" /></span>
+                        <span className="text-sm"><TextEmphasize data="science-backed" /></span>
+                        <span className="text-md"><TextEmphasize data="8 years" /></span>
+                        <span className="text-md"><TextEmphasize data="Level 4" /></span>
+                        <span className="text-md"><TextEmphasize data="personalized coaching" /></span> */}
                 </div>
 
 
